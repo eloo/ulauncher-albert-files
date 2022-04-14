b5ad53c0de732e4ec53875f9a60086c68e991fbc
Joseph Weigl <2631895+eloo@users.noreply.github.com>
2022-04-14 21:08:24 +0200

    Update icon.py

--- files/icon.py
+++ files/icon.py
@@ -9,6 +9,7 @@ if Launcher.get() == Launcher.ALBERT:
     except ImportError: pass
 elif Launcher.get() == Launcher.ULAUNCHER:
     try:
+        gi.require_version('Gtk', '4.0')
         from gi.repository import Gtk
         icon_theme = Gtk.IconTheme.get_default()
         def iconLookup(icon_name):
